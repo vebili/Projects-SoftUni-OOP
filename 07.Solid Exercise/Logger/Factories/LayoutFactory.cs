5a7273e0c46b2b811e34bec84f2cc24af20abf15
vebili <74007431+vebili@users.noreply.github.com>
2021-08-24 18:11:02 +0300

    Add

---
+++ 07.Solid Exercise/Logger/Factories/LayoutFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Logger.Models.Layouts;
+using Logger.Models.Contracts;
+using System.Reflection;
+using System.Linq;
+
+namespace Logger.Factories
+{
+   public class LayoutFactory
+    {
+
+        public ILayout ProduceLayout(string layoutType)
+        {
+            
+            Assembly assemby = Assembly.GetExecutingAssembly();
+            Type type = assemby
+                .GetTypes()
+                .FirstOrDefault(t => t.Name.ToLower() == layoutType.ToLower());
+
+            object[] args = new object[] { };
+
+            ILayout layout = (ILayout)Activator.CreateInstance(type, args);
+
+            return layout;
+        }
+    }
+}
